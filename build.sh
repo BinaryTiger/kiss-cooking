4f0a32c97730e76d873c6622559089987c0d8096
Antoine Racine <racine.antoine@gmail.com>
2020-04-06 16:17:47 -0400

    Add support for static files in build script and start work on recipe list

--- build.sh
+++ build.sh
@@ -1,25 +1,37 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
 DATA_DIR=./data
 BUILD_DIR=./build
+TEMP_DIR=./build/temp
+STATIC_DIR=./static
 RECIPE_TEMPLATE=./templates/recipe.mustache
 
 if [ -d "$BUILD_DIR" ]; then
     rm -rf $BUILD_DIR
 fi
 
-mkdir build
+mkdir $BUILD_DIR
+mkdir $TEMP_DIR
 
-hash_array=()
+declare -A hash_array
 
 # Build every recipe and keep their hash ref in the array
 for f in "$DATA_DIR"/*.dhall; do
     HASH=$(dhall hash --file $f)
-    hash_array+=${HASH:7}
+    NAME=$(basename -- $f .dhall)
+
+    hash_array[$NAME]=${HASH:7}
 
     dhall-to-yaml-ng --file $f | mustache - $RECIPE_TEMPLATE > $BUILD_DIR/${HASH:7}.html
 done
 
-for h in $hash_array; do
-    echo $h
+# Import all static files as is
+for f in $STATIC_DIR; do
+    cp "$f"/* $BUILD_DIR
+done
+
+echo "recipes:" >> $TEMP_DIR/recipes.yaml
+for h in "${!hash_array[@]}"; do
+    echo "- name: $h" >> $TEMP_DIR/recipes.yaml
+    echo "  hash: ${hash_array[$h]}" >> $TEMP_DIR/recipes.yaml
 done
