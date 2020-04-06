29479811ab5a8cde9efd20edd075facdeaa5f2c8
Antoine Racine <racine.antoine@gmail.com>
2020-04-06 15:18:29 -0400

    Add more output to build script

--- build.sh
+++ build.sh
@@ -10,7 +10,16 @@ fi
 
 mkdir build
 
+hash_array=()
+
+# Build every recipe and keep their hash ref in the array
 for f in "$DATA_DIR"/*.dhall; do
     HASH=$(dhall hash --file $f)
+    hash_array+=${HASH:7}
+
     dhall-to-yaml-ng --file $f | mustache - $RECIPE_TEMPLATE > $BUILD_DIR/${HASH:7}.html
+done
+
+for h in $hash_array; do
+    echo $h
 done
